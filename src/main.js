8a47eed8dadcfefad44604ac929aec31c8f23d75
Maxwell Meyer <maxreadswell@gmail.com>
2021-06-29 15:27:16 -0700

    fix api logic.
    Co-authored-by: Gurshan Kaile <gurshankaile206@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,4 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import DinoService from "../src/service.js";
 
-$;
+async function getDinos(input1, input2) {
+  let response = await DinoService.dinoIpsum(input1, input2);
+
+  if (response.result === "success") {
+    $(".result").text(response.body);
+    console.log("getDinos works..?");
+  }
+}
+
+$(document).ready(function () {
+  $("form#dinos").submit(function (event) {
+    event.preventDefault();
+    const input1 = $("#paragraphs").val();
+    const input2 = $("#words").val();
+    getDinos(input1, input2);
+  });
+});
--- src/service.js
+++ src/service.js
@@ -1,5 +1,5 @@
 export default class DinoService {
-  static async dinoIpsum() {
+  static async dinoIpsum(input1, input2) {
     try {
       let response = await fetch(
         `http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${input1}&words=${input2}`
@@ -7,6 +7,8 @@ export default class DinoService {
       if (!response.ok) {
         throw Error(response);
       }
+      console.log(response);
+      console.log("apicall");
       return response.json();
     } catch (error) {
       return `Oh no, there was an error ${error}`;
